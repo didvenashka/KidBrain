4bb2ef9301784c3bd0cbcc46286073c129e4a3c7
didvenashka <ytepol7@gmail.com>
2022-03-29 23:47:59 +0300

    Add dependency injection

---
+++ Assets/Scripts/DependencyResolver.cs
@@ -0,0 +1,31 @@
+﻿using Zenject;
+
+public class DependencyResolver : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<IMemorySquaresGameManager>()
+            .To<MemorySquaresGameManager>()
+            .AsSingle();
+
+        Container.Bind<IQuickCalculationsGameManager>()
+            .To<QuickCalculationsGameManager>()
+            .AsSingle();
+
+        Container.Bind<IQuickEyeGameManager>()
+            .To<QuickEyeGameManager>()
+            .AsSingle();
+
+        Container.Bind<IReactTapGameManager>()
+            .To<ReactTapGameManager>()
+            .AsSingle();
+
+        Container.Bind<IScoreManager>()
+            .To<ScoreManager>()
+            .AsSingle();
+
+        Container.Bind<IScoreRepository>()
+            .To<ScoreRepository>()
+            .AsSingle();
+    }
+}
